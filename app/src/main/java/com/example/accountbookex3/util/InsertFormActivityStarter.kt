fa12d6b591bfe43119fffd5ea959edabc5e02ff1
hyon2S <hhsim123@gmail.com>
2020-10-21 16:47:02 +0900

    Add TAG for Log

--- app/src/main/java/com/example/accountbookex3/util/InsertFormActivityStarter.kt
+++ app/src/main/java/com/example/accountbookex3/util/InsertFormActivityStarter.kt
@@ -2,9 +2,12 @@ package com.example.accountbookex3.util
 
 import android.app.Activity
 import android.content.Intent
+import android.util.Log
 import com.example.accountbookex3.activity.InsertFormActivity
 
 class InsertFormActivityStarter(private val currentActivity: Activity) {
+    private val TAG = "InsertFormActivityStarterLog"
+
     fun startActivity() {
         val insertIntent = Intent(currentActivity, InsertFormActivity::class.java)
         currentActivity.startActivity(insertIntent)
