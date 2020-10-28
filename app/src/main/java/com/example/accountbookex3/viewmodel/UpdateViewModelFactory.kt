b35d1a25109e906430924bd99cb1249e95596015
hyon2S <hhsim123@gmail.com>
2020-10-29 00:23:39 +0900

    UpdateViewModelFactory 클래스 추가

---
+++ app/src/main/java/com/example/accountbookex3/viewmodel/UpdateViewModelFactory.kt
@@ -0,0 +1,23 @@
+package com.example.accountbookex3.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.lang.IllegalArgumentException
+
+/*
+* https://readystory.tistory.com/176
+* 의 4번 방법 이용해서 만듦
+* */
+class UpdateViewModelFactory
+    (private val dbViewModel: DbViewModel, private val date: String, private val index: Int)
+    : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(UpdateViewModel::class.java)) {
+            UpdateViewModel(dbViewModel, date, index) as T
+        }
+        else {
+            throw IllegalArgumentException()
+        }
+    }
+
+}
