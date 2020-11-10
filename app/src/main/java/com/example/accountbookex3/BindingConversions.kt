9b91271d298a58108c97e10b6640407e8ba87e3f
hyon2S <hhsim123@gmail.com>
2020-11-10 17:15:26 +0900

    로그 추가

--- app/src/main/java/com/example/accountbookex3/BindingConversions.kt
+++ app/src/main/java/com/example/accountbookex3/BindingConversions.kt
@@ -1,13 +1,17 @@
 package com.example.accountbookex3
 
+import android.util.Log
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import java.time.LocalDate
 
 object BindingConversions {
+    const val TAG = "BindingConversionsLog"
+
     @BindingAdapter("android:text")
     @JvmStatic
     fun bindDate(textView: TextView, date: LocalDate) {
+        Log.d(TAG, "bindDate()")
         textView.text = date.toString()
     }
 }
