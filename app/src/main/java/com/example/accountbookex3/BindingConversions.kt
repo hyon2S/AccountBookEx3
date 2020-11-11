f5dcc2fb5499795750cfc67b7dc0f7e2cfea3cbc
hyon2S <hhsim123@gmail.com>
2020-11-11 18:07:09 +0900

    로그 주석 처리
    Binding이 일어날때마다 로그가 찍혀서 필요한 (다른) 로그를 찾는데 지장이 생김. 주석처리함.

--- app/src/main/java/com/example/accountbookex3/BindingConversions.kt
+++ app/src/main/java/com/example/accountbookex3/BindingConversions.kt
@@ -11,21 +11,21 @@ object BindingConversions {
     @BindingAdapter("android:text")
     @JvmStatic
     fun bindDate(textView: TextView, date: LocalDate) {
-        Log.d(TAG, "bindDate()")
+        // Log.d(TAG, "bindDate()")
         textView.text = date.toString()
     }
 
     @BindingAdapter("android:text")
     @JvmStatic
     fun bindAmount(textView: TextView, amount: Int) {
-        Log.d(TAG, "bindAmount()")
+        // Log.d(TAG, "bindAmount()")
         textView.text = amount.toString()
     }
 
     @BindingAdapter("android:text")
     @JvmStatic
     fun bindIncomeOutcome(textView: TextView, isIncome: Boolean) {
-        Log.d(TAG, "bindIncomeOutcome()")
+        // Log.d(TAG, "bindIncomeOutcome()")
         textView.text = if (isIncome)
             AccountBookApplication.applicationContext().getString(R.string.income)
         else
