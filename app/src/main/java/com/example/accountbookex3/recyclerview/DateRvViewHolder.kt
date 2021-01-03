0ad0b57d114c409cc8157e315596a8adf8937a46
hyon2S <hhsim123@gmail.com>
2021-01-03 17:59:17 +0900

    리사이클러뷰 드래그앤드롭, 스와이프 기능 삭제
    원래 계획은 리사이클러뷰의 내용물을 드래그앤드롭으로 순서 변경, 스와이프로 삭제 할 수 있게 하는 것이어서
    ItemTouchHelper.Callback()를 상속받는 ItemTouchHelperCallback 클래스를 만들어놓았음.
    
    둘 다 구현하지 않게 됨.
    아무 기능도 하지 않는 드래그앤드롭과 스와이프가 그대로 남아있어서 삭제함.
    
    ItemTouchHelperCallback, ItemTouchHelperAdapter 두 클래스 삭제

--- app/src/main/java/com/example/accountbookex3/recyclerview/DateRvViewHolder.kt
+++ app/src/main/java/com/example/accountbookex3/recyclerview/DateRvViewHolder.kt
@@ -1,10 +1,8 @@
 package com.example.accountbookex3.recyclerview
 
-import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.accountbookex3.data.DateRecord
 import com.example.accountbookex3.databinding.RecyclerViewDateBinding
-import com.example.accountbookex3.dragandswipe.ItemTouchHelperCallback
 import com.example.accountbookex3.edit.RvEditHelper
 
 class DateRvViewHolder(private val binding: RecyclerViewDateBinding): RecyclerView.ViewHolder(binding.root) {
@@ -16,8 +14,5 @@ class DateRvViewHolder(private val binding: RecyclerViewDateBinding): RecyclerVi
             adapter = recordAdapter
         }
         // 중첩구조니까 어쩔수없긴 한데 그래도 너무 심각하게 의존하는 구조가 아닌가 싶음..
-        val itemTouchHelperCallback = ItemTouchHelperCallback(recordAdapter)
-        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
-        itemTouchHelper.attachToRecyclerView(binding.rvInner)
     }
 }
--- app/src/main/java/com/example/accountbookex3/recyclerview/RecordRvAdapter.kt
+++ app/src/main/java/com/example/accountbookex3/recyclerview/RecordRvAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.accountbookex3.recyclerview
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -8,13 +7,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.accountbookex3.R
 import com.example.accountbookex3.data.Record
 import com.example.accountbookex3.databinding.RecyclerViewRecordBinding
-import com.example.accountbookex3.dragandswipe.ItemTouchHelperAdapter
 import com.example.accountbookex3.edit.RvEditHelper
 import io.realm.OrderedRealmCollection
 import java.time.LocalDate
 
 class RecordRvAdapter(val date: LocalDate, var data: OrderedRealmCollection<Record>, private val rvEditHelper: RvEditHelper)
-    : RecyclerView.Adapter<RecordRvViewHolder>(), ItemTouchHelperAdapter {
+    : RecyclerView.Adapter<RecordRvViewHolder>() {
     private val TAG = "RecordRvAdapterLog"
 
     override fun getItemCount(): Int = data.size
@@ -28,15 +26,5 @@ class RecordRvAdapter(val date: LocalDate, var data: OrderedRealmCollection<Reco
     override fun onBindViewHolder(holder: RecordRvViewHolder, position: Int) {
         val record: Record = data.get(position) ?: return
         holder.bind(record, date, position)
-    }
-
-    override fun onItemMove(fromPosition: Int, toPosition: Int) {
-        Log.d(TAG, "onItemMove(${date}: ${fromPosition} -> ${toPosition})")
-        // dbViewModel.moveRecord(date, fromPosition, toPosition)
-    }
-
-    override fun onItemDeleteCheck(position: Int) {
-        Log.d(TAG, "onItemDeleteCheck(${date}: ${position})")
-        // dbViewModel.delete(date, position)
     }
 }
